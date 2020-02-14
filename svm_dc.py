15f223420eadf3adc9bdf3b9ecbb827ee89f9cce
Yinbin Ma <me@yinbin.ma>
2020-02-13 19:42:40 -0600

    fix bugs

--- svm_dc.py
+++ svm_dc.py
@@ -1,53 +1,12 @@
 from mpi4py import MPI
 import numpy as np
 import cvxpy as cp
-import matplotlib.pyplot as plt
-import time
+from consensus import GCon, matrix
 
-def ring_matrix(n):
-    w = np.eye(n) * 0.5
-    for i in range(n):
-        w[i, (i+1)%n] = 0.25
-        w[i, (i-1)%n] = 0.25
-    return w
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
-
-class GCon(object):
-    """
-    Decentralized consensus module based on MPI
-    """
-    def __init__(self, features, w):
-        self.x_last = np.zeros(features)
-        self.s_last = np.zeros(features)
-        self.w = w
-        # target and buf are for exchanging data
-        self.target = []
-        self.rank = MPI.COMM_WORLD.Get_rank()
-        for i, ws in enumerate(w):
-            if ws != 0 and i != self.rank:
-                self.target.extend([i])
-        self.buf = np.zeros([len(self.target), features])
-    
-    def __call__(self, x):
-        requests = []
-        for i, tar in enumerate(self.target):
-            requests.extend([
-                comm.Isend(self.s_last * self.w[tar], tar), 
-                comm.Irecv(self.buf[i], tar)
-            ])
-        # Wait all requests finished
-        MPI.Request().Waitall(requests)
-        # Updating formula, see Shuo Han's CSL letter, formula (3)
-        result = np.sum(self.buf, axis=0) + self.w[self.rank] * \
-            self.s_last + (x - self.x_last)
-        self.x_last, self.s_last = x, result
-        return result
-
-
 
 M, batch = 2, 20
 A = np.zeros([batch*2, M+1])
@@ -73,7 +32,7 @@ if rank == 0:
     AA = np.stack([AA[random_indexes[idx], :] for idx in range(size)])
 
     # Generate W, here we use ring.
-    WW = ring_matrix(size)
+    WW = matrix.ring_matrix(size)
 
 comm.Scatter(AA, A, root=0)
 comm.Scatter(WW, W, root=0)
@@ -81,9 +40,7 @@ comm.Scatter(WW, W, root=0)
 
 # Training period
 
-MAX_ITER = 1000 + 1
-ABSTOL = 1e-4
-RELTOL = 1e-2
+MAX_ITER = 500 + 1
 rho, C = 1, 1
 
 z, u, x = np.zeros(M+1), np.zeros(M+1), np.zeros(M+1)
@@ -104,10 +61,11 @@ while True:
     x = x_var.value
 
     x_ave = x_con(x)
-    z = size * rho/(1/C + size*rho) * (x_ave + u_con(u))
-    u += x - z
+    u_ave = u_con(u)
+    z = size * rho/(1/C + size*rho) * (x_ave + u_ave)
+    u = u + x - z
     
-    if i % 50 == 0:
-        print(rank, i, x_ave, u, z)
+    if i % 100 == 0:
+        print(rank, i, x, z, u)
 
     i += 1
